c496733563441dbf2fce56952fe1c4ce0db557ef
Phisict <adrianfathan69@gmail.com>
2024-12-03 21:53:28 +0700

    Menambah Web Interface

---
+++ mainProgram/Web.py
@@ -0,0 +1,41 @@
+# Inisialisasi Trie
+from unittest import result
+from sklearn.utils import resample
+from Trie import Trie, Data
+from flask import Flask, jsonify,render_template
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# Inisialisasi Trie dan HashMap
+data = Data()
+buku = data.get_data()
+trie = Trie()
+
+
+# Struktur Buku : {'Kode Buku': {'Judul', 'Penulis', 'Tahun-Publikasi', 'Penerbit', 'Url}  }
+for key, value in buku.items():
+    # Memasukkan Judul dan Key Buku ke Trie
+    trie.insert(value['Judul'], key)
+    
+
+@app.route('/search/<nama>', methods=['GET'])
+def search(nama):
+    print(nama)
+    if not nama:
+        return jsonify({'error': 'Query parameter is required'}), 400
+    
+    results = trie.search(nama)
+    if not results:
+        return jsonify({'message': 'No results found'}), 404
+    results = [buku[i[0]] for i in results[:10]]
+    return jsonify({'results': results})
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=6969)
